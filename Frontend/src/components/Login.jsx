02b7cd905a465fbf26f79ff8c670457628b8a1e0
Guruprasad Bhat <guruprasadbhat106@gmail.com>
2024-11-14 18:59:41 +0530

    edit the auth pages

--- Frontend/src/components/Login.jsx
+++ Frontend/src/components/Login.jsx
@@ -9,48 +9,36 @@ import axios from "axios";
 import { USER_API_END_POINT } from "@/utils/constant";
 import { toast } from "sonner";
 import { useDispatch, useSelector } from "react-redux";
-import { setLoading } from "@/redux/authSlice";
+import { setLoading, setUser } from "@/redux/authSlice";
 import { Loader2 } from "lucide-react";
 
-const Signup = () => {
+const Login = () => {
   const [input, setInput] = useState({
-    fullname: "",
     email: "",
-    phoneNumber: "",
     password: "",
     role: "",
-    file: "",
   });
   const { loading, user } = useSelector((store) => store.auth);
-  const dispatch = useDispatch();
   const navigate = useNavigate();
+  const dispatch = useDispatch();
 
   const changeEventHandler = (e) => {
     setInput({ ...input, [e.target.name]: e.target.value });
   };
-  const changeFileHandler = (e) => {
-    setInput({ ...input, file: e.target.files?.[0] });
-  };
+
   const submitHandler = async (e) => {
     e.preventDefault();
-    const formData = new FormData(); //formdata object
-    formData.append("fullname", input.fullname);
-    formData.append("email", input.email);
-    formData.append("phoneNumber", input.phoneNumber);
-    formData.append("password", input.password);
-    formData.append("role", input.role);
-    if (input.file) {
-      formData.append("file", input.file);
-    }
-
     try {
       dispatch(setLoading(true));
-      const res = await axios.post(`${USER_API_END_POINT}/register`, formData, {
-        headers: { "Content-Type": "multipart/form-data" },
+      const res = await axios.post(`${USER_API_END_POINT}/login`, input, {
+        headers: {
+          "Content-Type": "application/json",
+        },
         withCredentials: true,
       });
       if (res.data.success) {
-        navigate("/login");
+        dispatch(setUser(res.data.user));
+        navigate("/");
         toast.success(res.data.message);
       }
     } catch (error) {
@@ -60,7 +48,6 @@ const Signup = () => {
       dispatch(setLoading(false));
     }
   };
-
   useEffect(() => {
     if (user) {
       navigate("/");
@@ -74,17 +61,7 @@ const Signup = () => {
           onSubmit={submitHandler}
           className="w-1/2 border border-gray-200 rounded-md p-4 my-10"
         >
-          <h1 className="font-bold text-xl mb-5">Sign Up</h1>
-          <div className="my-2">
-            <Label>Full Name</Label>
-            <Input
-              type="text"
-              value={input.fullname}
-              name="fullname"
-              onChange={changeEventHandler}
-              placeholder="patel"
-            />
-          </div>
+          <h1 className="font-bold text-xl mb-5">Login</h1>
           <div className="my-2">
             <Label>Email</Label>
             <Input
@@ -95,16 +72,7 @@ const Signup = () => {
               placeholder="patel@gmail.com"
             />
           </div>
-          <div className="my-2">
-            <Label>Phone Number</Label>
-            <Input
-              type="text"
-              value={input.phoneNumber}
-              name="phoneNumber"
-              onChange={changeEventHandler}
-              placeholder="8080808080"
-            />
-          </div>
+
           <div className="my-2">
             <Label>Password</Label>
             <Input
@@ -140,15 +108,6 @@ const Signup = () => {
                 <Label htmlFor="r2">Recruiter</Label>
               </div>
             </RadioGroup>
-            <div className="flex items-center gap-2">
-              <Label>Profile</Label>
-              <Input
-                accept="image/*"
-                type="file"
-                onChange={changeFileHandler}
-                className="cursor-pointer"
-              />
-            </div>
           </div>
           {loading ? (
             <Button className="w-full my-4">
@@ -157,13 +116,13 @@ const Signup = () => {
             </Button>
           ) : (
             <Button type="submit" className="w-full my-4">
-              Signup
+              Login
             </Button>
           )}
           <span className="text-sm">
-            Already have an account?{" "}
-            <Link to="/login" className="text-blue-600">
-              Login
+            Don't have an account?{" "}
+            <Link to="/signup" className="text-blue-600">
+              Signup
             </Link>
           </span>
         </form>
@@ -172,4 +131,4 @@ const Signup = () => {
   );
 };
 
-export default Signup;
+export default Login;
